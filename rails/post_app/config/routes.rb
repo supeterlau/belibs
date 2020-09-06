b4dd49eef35c291efd782107132f7b64c5506bde
Peter Lau <superpeterlau@outlook.com>
2020-09-06 23:10:03 +0800

    ruby: rails: init post_app

---
+++ rails/post_app/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # app/controllers/application_controller.rb
+  # path /
+  root 'application#hello'
+end
