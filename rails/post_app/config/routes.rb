3fd08fc0a6505b9a1f9797ccb736f75271f2028a
Peter Lau <superpeterlau@outlook.com>
2020-09-10 18:21:35 +0800

    ruby: rails: ch2 2.3 show first micropost

--- rails/post_app/config/routes.rb
+++ rails/post_app/config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  resources :microposts
   resources :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # app/controllers/application_controller.rb
   # path /
-  root 'application#hello'
+  # root 'application#hello'
+
+  root 'users#index'
 end
