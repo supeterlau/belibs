7c8c0ff39c6e2afe181308e3d790833039a9e90c
Peter Lau <superpeterlau@outlook.com>
2021-08-02 01:04:19 +0800

    php: laravel tour

---
+++ php/laravel/tour/resources/views/task/edit.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+<h2>Edit Task</h2>
+<form method="POST" action="/task/{{$task->id}}">
+<div class="form-group">
+<textarea name="description" class="form-control">{{$task->description}}</textarea>
+</div>
+<div class="form-group">
+<button type="submit" class="btn btn-primary">Update Task</button>
+</div>
+{{ csrf_field() }}
+</form>
+
+@endsection
